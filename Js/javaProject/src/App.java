bc238b1b5cd012317c80b4e11d76a5ddc010fea9
Sebastian171962 <88748607+Sebastian171962@users.noreply.github.com>
2021-10-23 13:13:17 -0500

    Update App.java

--- Js/javaProject/src/App.java
+++ Js/javaProject/src/App.java
@@ -3,6 +3,12 @@ import java.util.GregorianCalendar;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
+public class App {
 
+    public static void main(String[] args) throws Exception {                           //Inicio
+
+        Scanner user = new Scanner(System.in);
+        String name;
+        int eleccion1 = 0;
 
 
